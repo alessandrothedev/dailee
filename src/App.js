e718469bf7393f3942db17c26e697cb29e996f70
alessandrothedev <spiridigliozzi.alessandro@gmail.com>
2022-01-15 18:57:12 +0000

    fetching weatherAPI and getting user's location

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react'
 
 function App() {
+
+    const [latitude, setLatitude] = useState(0)
+    const [longitude, setLongitude] = useState(0)
+    const [location, setLocation] = useState([])
+
+    navigator.geolocation.getCurrentPosition( (position) => {
+        setLatitude(position.coords.latitude)
+        setLongitude(position.coords.longitude)
+    })
+
+    useEffect(() => {
+        fetch(`https://api.weatherapi.com/v1/forecast.json?key=db698b5e650a441fae6190451221401&q=${latitude},${longitude}&days=1&aqi=yes&alerts=yes`)
+        .then(response => response.json())
+        .then(data => { 
+            const locationData = data;
+            setLocation(locationData);
+        });
+    }, [])
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Latitude: {latitude}</h2>
+      <h2>Longitude: {longitude}</h2>
+      {latitude > 0 && <h3>{location.location.name}</h3>}
+      {latitude > 0 && <h3>{location.location.country}</h3>}
     </div>
   );
 }
